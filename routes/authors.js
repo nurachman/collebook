26b0d976de5046994e12538420c7b5b093cba0d5
Your Name <you@example.com>
2021-01-15 12:41:10 +0700

    Debug Author Edit

--- routes/authors.js
+++ routes/authors.js
@@ -58,7 +58,7 @@ router.get('/:id', async (req, res) => {
 router.get('/:id/edit', async (req, res) => {
     try {  
         const author = await Author.findById(req.params.id)
-        res.render('authors/edit', { author: new Author() })
+        res.render('authors/edit', { author: author })
     } catch {
         res.redirect('/')
     }
